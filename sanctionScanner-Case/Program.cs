7009b5234184683a552816d7a1a931708b2106b0
ALEYNA YEŞİL <aleynayesil2304@outlook.com>
2022-03-24 09:55:25 +0300

    Add project files.

---
+++ sanctionScanner-Case/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sanctionScanner_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
